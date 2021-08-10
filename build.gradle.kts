0cb13f32b259bbec3dd8107057c2ac8c9baf5c61
limbang <495071565@qq.com>
2021-08-10 19:39:53 +0800

    ci: githubPackagesRegistry build: 添加依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
 plugins {
     val kotlinVersion = "1.5.21"
     kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+    `maven-publish`
 }
 
-group = "top.limbang.yggdrasil"
+group = "top.limbang.minecraft"
 version = "1.0.0"
 
 repositories {
@@ -14,7 +17,19 @@ repositories {
 }
 
 dependencies {
+    val coroutinesVersion = "1.5.1"
+    val retrofitVersion = "2.9.0"
+    val okhttpVersion = "5.0.0-alpha.2"
+    implementation("org.slf4j:slf4j-api:2.0.0-alpha2")
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
+    implementation("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+
     testImplementation(kotlin("test"))
+    testImplementation("ch.qos.logback:logback-classic:1.3.0-alpha6")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:$coroutinesVersion")
 }
 
 tasks.test {
@@ -22,5 +37,27 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions{
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs.plus("-Xopt-in=kotlin.RequiresOptIn")
+    }
+}
+
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Forsaken-Land/yggdrasil")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
