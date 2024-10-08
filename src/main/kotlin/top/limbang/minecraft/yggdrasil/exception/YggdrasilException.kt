8e5e900d7dcf0d1908bf411e65c15d0e7d7522c9
limbang <495071565@qq.com>
2024-10-08 21:33:28 +0800

    docs: 更新版权声明

--- src/main/kotlin/top/limbang/minecraft/yggdrasil/exception/YggdrasilException.kt
+++ src/main/kotlin/top/limbang/minecraft/yggdrasil/exception/YggdrasilException.kt
@@ -1,10 +1,8 @@
 /*
- * Copyright 2021 Forsaken-Land and contributors.
+ * Copyright 2024 limbang and contributors.
  *
- * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
- *
- * https://github.com/Forsaken-Land/yggdrasil/blob/master/LICENSE
+ * 此源代码的使用受 GNU AGPLv3 许可证的约束，该许可证可在"LICENSE"文件中找到。
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found in the "LICENSE" file.
  */
 
 package top.limbang.minecraft.yggdrasil.exception
